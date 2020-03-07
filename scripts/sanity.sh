2d008f43ad94dec62f969c75af8e38880f8d2ac0
tom doron <tomer@apple.com>
2020-03-06 18:39:26 -0800

    headers, santity fix

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -19,7 +19,7 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function replace_acceptable_years() {
     # this needs to replace all acceptable forms with 'YEARS'
-    sed -e 's/2017-2018/YEARS/' -e 's/2017-2019/YEARS/' -e 's/2019/YEARS/'
+    sed -e 's/2017-2018/YEARS/' -e 's/2017-2019/YEARS/' -e 's/2019/YEARS/' -e 's/2020/YEARS/'
 }
 
 printf "=> Checking linux tests... "
