dbbcb078330049748f8f5283e9ef32df7cf4e93b
tom doron <tomer@apple.com>
2020-03-11 18:52:57 -0700

    improve linux perf script
    motivation: more details from perf / flamegrpah
    
    changes:
    * run docker in privileged mode and turn off kptr_restrict
    * fix typo

--- scripts/linux_performance_setup.sh
+++ scripts/linux_performance_setup.sh
@@ -13,17 +13,19 @@
 ##
 ##===----------------------------------------------------------------------===##
 
-# docker run --cap-add SYS_ADMIN -it -v `pwd`:/code -w /code swift:5.1 bash
+# docker run --privileged -it -v `pwd`:/code -w /code swift:5.1 bash
 
-apt-get update
+apt-get update -y
 apt-get install -y vim htop strace linux-tools-common linux-tools-generic
+
+echo 0 > /proc/sys/kernel/kptr_restrict
 
 cd /usr/bin
 rm -rf perf
 ln -s /usr/lib/linux-tools/4.15.0-88-generic/perf perf
 cd -
 
-cd build
+cd /opt
 git clone https://github.com/brendangregg/FlameGraph.git
 cd -
 
@@ -40,5 +42,5 @@ cd -
 #
 # perf
 # export MAX_REQUESTS=10000
-# perf record -o .build/perf-$MAX_REQUESTS.data -g .build/release/SwiftAwsLambdaStringSample dward
-# perf script -i .build/perf-$MAX_REQUESTS.data | .build/FlameGraph/stackcollapse-perf.pl | swift-demangle | .build/FlameGraph/flamegraph.pl > .build/flamegraph-$MAX_REQUESTS.svg
+# perf record -o .build/perf-$MAX_REQUESTS.data -g .build/release/SwiftAwsLambdaStringSample dwarf
+# perf script -i .build/perf-$MAX_REQUESTS.data | /opt/FlameGraph/stackcollapse-perf.pl | swift-demangle | /opt/FlameGraph/flamegraph.pl > .build/flamegraph-$MAX_REQUESTS.svg
