ab284e658077bd2e0bb7089beeb136542fbb59f4
Fabian Fett <fabianfett@mac.com>
2020-03-24 17:28:39 +0100

    With propertyWrapper.

---
+++ Sources/AWSLambdaEvents/DateWrappers.swift
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2017-2020 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import struct Foundation.Date
+import class Foundation.ISO8601DateFormatter
+
+@propertyWrapper
+public struct ISO8601WithFractionalSecondsCoding: Decodable {
+    public let wrappedValue: Date
+
+    public init(wrappedValue: Date) {
+        self.wrappedValue = wrappedValue
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let dateString = try container.decode(String.self)
+        guard let date = Self.dateFormatter.date(from: dateString) else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription:
+                "Expected date to be in iso8601 date format with fractional seconds, but `\(dateString) does not forfill format`")
+        }
+        self.wrappedValue = date
+    }
+
+    private static let dateFormatter: ISO8601DateFormatter = Self.createDateFormatter()
+    private static func createDateFormatter() -> ISO8601DateFormatter {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [
+            .withInternetDateTime,
+            .withDashSeparatorInDate,
+            .withColonSeparatorInTime,
+            .withColonSeparatorInTimeZone,
+            .withFractionalSeconds,
+        ]
+        return formatter
+    }
+}
--- Sources/AWSLambdaEvents/S3.swift
+++ Sources/AWSLambdaEvents/S3.swift
@@ -13,16 +13,17 @@
 //===----------------------------------------------------------------------===//
 
 import struct Foundation.Date
-import class Foundation.ISO8601DateFormatter
 
 // https://github.com/aws/aws-lambda-go/blob/master/events/s3.go
 public enum S3 {
     public struct Event: Decodable {
-        public struct Record {
+        public struct Record: Decodable {
             public let eventVersion: String
             public let eventSource: String
             public let awsRegion: String
-            public let eventTime: Date
+
+            @ISO8601WithFractionalSecondsCoding
+            public var eventTime: Date
             public let eventName: String
             public let userIdentity: UserIdentity
             public let requestParameters: RequestParameters
@@ -72,53 +73,5 @@ public enum S3 {
         public let versionId: String?
         public let eTag: String
         public let sequencer: String
-    }
-}
-
-extension S3.Event.Record: Decodable {
-    enum CodingKeys: String, CodingKey {
-        case eventVersion
-        case eventSource
-        case awsRegion
-        case eventTime
-        case eventName
-        case userIdentity
-        case requestParameters
-        case responseElements
-        case s3
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        self.eventVersion = try container.decode(String.self, forKey: .eventVersion)
-        self.eventSource = try container.decode(String.self, forKey: .eventSource)
-        self.awsRegion = try container.decode(String.self, forKey: .awsRegion)
-
-        let dateString = try container.decode(String.self, forKey: .eventTime)
-        guard let timestamp = S3.Event.Record.dateFormatter.date(from: dateString) else {
-            throw DecodingError.dataCorruptedError(forKey: .eventTime, in: container, debugDescription:
-                "Expected date to be in iso8601 date format with fractional seconds, but `\(dateString) does not forfill format`")
-        }
-        self.eventTime = timestamp
-
-        self.eventName = try container.decode(String.self, forKey: .eventName)
-        self.userIdentity = try container.decode(S3.UserIdentity.self, forKey: .userIdentity)
-        self.requestParameters = try container.decode(S3.RequestParameters.self, forKey: .requestParameters)
-        self.responseElements = try container.decodeIfPresent([String: String].self, forKey: .responseElements) ?? [:]
-        self.s3 = try container.decode(S3.Entity.self, forKey: .s3)
-    }
-
-    private static let dateFormatter: ISO8601DateFormatter = S3.Event.Record.createDateFormatter()
-    private static func createDateFormatter() -> ISO8601DateFormatter {
-        let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [
-            .withInternetDateTime,
-            .withDashSeparatorInDate,
-            .withColonSeparatorInTime,
-            .withColonSeparatorInTimeZone,
-            .withFractionalSeconds,
-        ]
-        return formatter
     }
 }
