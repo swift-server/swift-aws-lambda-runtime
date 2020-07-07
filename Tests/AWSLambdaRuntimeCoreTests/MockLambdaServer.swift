e99002920cdc265ce1f36695c6cfd63ba6e73ded
Michal A <michal@pokryfka.net>
2020-07-07 15:17:30 +0800

    Generate XRay trace ID in LambdaRuntimeClientTest

--- Tests/AWSLambdaRuntimeCoreTests/MockLambdaServer.swift
+++ Tests/AWSLambdaRuntimeCoreTests/MockLambdaServer.swift
@@ -144,7 +144,7 @@ internal final class HTTPHandler: ChannelInboundHandler {
                 responseHeaders = [
                     (AmazonHeaders.requestID, requestId),
                     (AmazonHeaders.invokedFunctionARN, "arn:aws:lambda:us-east-1:123456789012:function:custom-runtime"),
-                    (AmazonHeaders.traceID, "Root=1-5bef4de7-ad49b0e87f6ef6c87fc2e700;Parent=9a9197af755a6419;Sampled=1"),
+                    (AmazonHeaders.traceID, "Root=\(AmazonHeaders.generateXRayTraceID());Sampled=1"),
                     (AmazonHeaders.deadline, String(deadline)),
                 ]
             case .failure(let error):
