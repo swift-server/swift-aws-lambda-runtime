9166653bd8ef248a4326df1e56b14b3f72856646
tomer doron <tomerd@apple.com>
2021-03-04 19:16:07 -0800

    update code of conduct (#190)
    motivation: align with swift project coc update
    
    changes: update coc to the version used by the swift project (contributor-covenant.org  1.4)
    
    Update 0226-package-manager-target-based-dep-resolution.md
    
    Co-authored-by: Konrad `ktoso` Malawski <ktoso@apple.com>

--- scripts/soundness.sh
+++ scripts/soundness.sh
@@ -31,7 +31,7 @@ unacceptable_terms=(
     -e slav[e]
     -e sanit[y]
 )
-if git grep --color=never -i "${unacceptable_terms[@]}" > /dev/null; then
+if git grep --color=never -i "${unacceptable_terms[@]}" -- . ":(exclude)CODE_OF_CONDUCT.md" > /dev/null; then
     printf "\033[0;31mUnacceptable language found.\033[0m\n"
     git grep -i "${unacceptable_terms[@]}"
     exit 1
