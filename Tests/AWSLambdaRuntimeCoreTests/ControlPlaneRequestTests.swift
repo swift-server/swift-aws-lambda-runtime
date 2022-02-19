c1f694f35ae9a273919d1978111f32309e749617
Stefan Nienhuis <stefan@nienhuisdevelopment.com>
2022-02-19 23:16:51 +0100

    Add default value for traceID header (#246)
    * Add default value for traceID header
    * Implement Invocation traceID test

---
+++ Tests/AWSLambdaRuntimeCoreTests/ControlPlaneRequestTests.swift
@@ -0,0 +1,37 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2022 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@testable import AWSLambdaRuntimeCore
+import NIOHTTP1
+import XCTest
+
+class InvocationTest: XCTestCase {
+    func testInvocationTraceID() throws {
+        let headers = HTTPHeaders([
+            (AmazonHeaders.requestID, "test"),
+            (AmazonHeaders.deadline, String(Date(timeIntervalSinceNow: 60).millisSinceEpoch)),
+            (AmazonHeaders.invokedFunctionARN, "arn:aws:lambda:us-east-1:123456789012:function:custom-runtime"),
+        ])
+
+        var invocation: Invocation?
+
+        XCTAssertNoThrow(invocation = try Invocation(headers: headers))
+        XCTAssertNotNil(invocation)
+
+        guard !invocation!.traceID.isEmpty else {
+            XCTFail("Invocation traceID is empty")
+            return
+        }
+    }
+}
