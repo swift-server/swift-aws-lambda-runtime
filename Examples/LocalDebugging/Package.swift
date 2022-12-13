ad11f44f496ae2e2de43c5e04ab4da933ae8d55f
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-12-13 13:19:04 +0100

    re arrange package structure to work with archive plugin

--- Examples/LocalDebugging/Package.swift
+++ Examples/LocalDebugging/Package.swift
@@ -6,25 +6,30 @@ let package = Package(
     name: "MyLambda",
     platforms: [
         .macOS(.v12),
+        .iOS(.v15)
     ],
     products: [
         .executable(name: "MyLambda", targets: ["MyLambda"]),
+        .library(name: "Shared", targets: ["Shared"]),
     ],
     dependencies: [
         // this is the dependency on the swift-aws-lambda-runtime library
         // in real-world projects this would say
-        // .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "1.0.0")
-        .package(name: "swift-aws-lambda-runtime", path: "../../.."),
-        .package(name: "Shared", path: "../Shared"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", branch: "main"),
+//        .package(name: "swift-aws-lambda-runtime", path: "../../.."),
     ],
     targets: [
         .executableTarget(
             name: "MyLambda",
             dependencies: [
                 .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
-                .product(name: "Shared", package: "Shared"),
+                .byName(name: "Shared"),
             ],
-            path: "."
+            path: "./MyLambda",
+            exclude: ["scripts/", "Dockerfile"]
         ),
+        .target(name: "Shared", 
+                dependencies: [],
+                path: "./Shared") 
     ]
 )
