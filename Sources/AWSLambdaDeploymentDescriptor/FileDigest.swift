f5583751121b3342a99dabd05da34a85012c5367
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2023-04-13 10:34:30 +0200

    apply swift format

--- Sources/AWSLambdaDeploymentDescriptor/FileDigest.swift
+++ Sources/AWSLambdaDeploymentDescriptor/FileDigest.swift
@@ -12,61 +12,61 @@
 //
 // ===----------------------------------------------------------------------===//
 
-import Foundation
 import CryptoKit
+import Foundation
 
 class FileDigest {
-    
-    public static func hex(from filePath: String?) -> String? {
-        guard let fp = filePath else {
-            return nil
-        }
-        
-        return try? FileDigest().update(path: fp).finalize()
+
+  public static func hex(from filePath: String?) -> String? {
+    guard let fp = filePath else {
+      return nil
     }
-    
-    enum InputStreamError: Error {
-        case createFailed(String)
-        case readFailed
+
+    return try? FileDigest().update(path: fp).finalize()
+  }
+
+  enum InputStreamError: Error {
+    case createFailed(String)
+    case readFailed
+  }
+
+  private var digest = SHA256()
+
+  func update(path: String) throws -> FileDigest {
+    guard let inputStream = InputStream(fileAtPath: path) else {
+      throw InputStreamError.createFailed(path)
     }
-    
-    private var digest = SHA256()
-    
-    func update(path: String) throws -> FileDigest  {
-        guard let inputStream = InputStream(fileAtPath: path) else {
-            throw InputStreamError.createFailed(path)
-        }
-        return try update(inputStream: inputStream)
+    return try update(inputStream: inputStream)
+  }
+
+  private func update(inputStream: InputStream) throws -> FileDigest {
+    inputStream.open()
+    defer {
+      inputStream.close()
     }
-    
-    private func update(inputStream: InputStream) throws -> FileDigest {
-        inputStream.open()
-        defer {
-            inputStream.close()
-        }
-        
-        let bufferSize = 4096
-        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
-        var bytesRead = inputStream.read(buffer, maxLength: bufferSize)
-        while bytesRead > 0 {
-            self.update(bytes: buffer, length: bytesRead)
-            bytesRead = inputStream.read(buffer, maxLength: bufferSize)
-        }
-        if bytesRead < 0 {
-            // Stream error occured
-            throw (inputStream.streamError ?? InputStreamError.readFailed)
-        }
-        return self
+
+    let bufferSize = 4096
+    let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+    var bytesRead = inputStream.read(buffer, maxLength: bufferSize)
+    while bytesRead > 0 {
+      self.update(bytes: buffer, length: bytesRead)
+      bytesRead = inputStream.read(buffer, maxLength: bufferSize)
     }
-    
-    private func update(bytes: UnsafeMutablePointer<UInt8>, length: Int) {
-        let data = Data(bytes: bytes, count: length)
-        digest.update(data: data)
+    if bytesRead < 0 {
+      // Stream error occured
+      throw (inputStream.streamError ?? InputStreamError.readFailed)
     }
-    
-    func finalize() -> String {
-        let digest = digest.finalize()
-        return digest.compactMap { String(format: "%02x", $0) }.joined()
-    }
-    
+    return self
+  }
+
+  private func update(bytes: UnsafeMutablePointer<UInt8>, length: Int) {
+    let data = Data(bytes: bytes, count: length)
+    digest.update(data: data)
+  }
+
+  func finalize() -> String {
+    let digest = digest.finalize()
+    return digest.compactMap { String(format: "%02x", $0) }.joined()
+  }
+
 }
