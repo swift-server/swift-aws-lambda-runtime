47cf4dc18344e7953df2539aa98bb6685fa25fd8
Fabian Fett <fabianfett@apple.com>
2024-08-26 15:00:49 +0200

    Ignore Package@swift-* to `.licenseignore`

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -27,47 +27,66 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", exact: "1.3.0"),
     ],
     targets: [
-        .target(name: "AWSLambdaRuntime", dependencies: [
-            .byName(name: "AWSLambdaRuntimeCore"),
-            .product(name: "NIOCore", package: "swift-nio"),
-            .product(name: "NIOFoundationCompat", package: "swift-nio"),
-        ]),
-        .target(name: "AWSLambdaRuntimeCore", dependencies: [
-            .product(name: "Logging", package: "swift-log"),
-            .product(name: "Backtrace", package: "swift-backtrace"),
-            .product(name: "NIOHTTP1", package: "swift-nio"),
-            .product(name: "NIOCore", package: "swift-nio"),
-            .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
-            .product(name: "NIOPosix", package: "swift-nio"),
-        ]),
+        .target(
+            name: "AWSLambdaRuntime",
+            dependencies: [
+                .byName(name: "AWSLambdaRuntimeCore"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio"),
+            ]
+        ),
+        .target(
+            name: "AWSLambdaRuntimeCore",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Backtrace", package: "swift-backtrace"),
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ]
+        ),
         .plugin(
             name: "AWSLambdaPackager",
             capability: .command(
                 intent: .custom(
                     verb: "archive",
-                    description: "Archive the Lambda binary and prepare it for uploading to AWS. Requires docker on macOS or non Amazonlinux 2 distributions."
+                    description:
+                        "Archive the Lambda binary and prepare it for uploading to AWS. Requires docker on macOS or non Amazonlinux 2 distributions."
                 )
             )
         ),
-        .testTarget(name: "AWSLambdaRuntimeCoreTests", dependencies: [
-            .byName(name: "AWSLambdaRuntimeCore"),
-            .product(name: "NIOTestUtils", package: "swift-nio"),
-            .product(name: "NIOFoundationCompat", package: "swift-nio"),
-        ]),
-        .testTarget(name: "AWSLambdaRuntimeTests", dependencies: [
-            .byName(name: "AWSLambdaRuntimeCore"),
-            .byName(name: "AWSLambdaRuntime"),
-        ]),
+        .testTarget(
+            name: "AWSLambdaRuntimeCoreTests",
+            dependencies: [
+                .byName(name: "AWSLambdaRuntimeCore"),
+                .product(name: "NIOTestUtils", package: "swift-nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio"),
+            ]
+        ),
+        .testTarget(
+            name: "AWSLambdaRuntimeTests",
+            dependencies: [
+                .byName(name: "AWSLambdaRuntimeCore"),
+                .byName(name: "AWSLambdaRuntime"),
+            ]
+        ),
         // testing helper
-        .target(name: "AWSLambdaTesting", dependencies: [
-            .byName(name: "AWSLambdaRuntime"),
-            .product(name: "NIO", package: "swift-nio"),
-        ]),
+        .target(
+            name: "AWSLambdaTesting",
+            dependencies: [
+                .byName(name: "AWSLambdaRuntime"),
+                .product(name: "NIO", package: "swift-nio"),
+            ]
+        ),
         .testTarget(name: "AWSLambdaTestingTests", dependencies: ["AWSLambdaTesting"]),
         // for perf testing
-        .executableTarget(name: "MockServer", dependencies: [
-            .product(name: "NIOHTTP1", package: "swift-nio"),
-            .product(name: "NIO", package: "swift-nio"),
-        ]),
+        .executableTarget(
+            name: "MockServer",
+            dependencies: [
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIO", package: "swift-nio"),
+            ]
+        ),
     ]
 )
