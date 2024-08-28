3831346770f0042f5b42bb70c5f7ed60b44b27df
Fabian Fett <fabianfett@apple.com>
2024-08-28 23:49:20 +0200

    Add `NewLambdaRuntimeError`

---
+++ Sources/AWSLambdaRuntimeCore/LambdaRuntimeError.swift
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2024 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+struct NewLambdaRuntimeError: Error {
+    enum Code {
+        case writeAfterFinishHasBeenSent
+        case finishAfterFinishHasBeenSent
+        case lostConnectionToControlPlane
+        case unexpectedStatusCodeForRequest
+        
+    }
+
+    var code: Code
+
+
+}
