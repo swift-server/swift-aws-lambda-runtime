6f384ae9697588784f082e763f611844c2986162
Aryan Shah <aryan_shah@apple.com>
2024-09-04 10:15:05 +0100

    Add LambdaJSONOutputEncoder

--- Sources/AWSLambdaRuntime/Lambda+Codable.swift
+++ Sources/AWSLambdaRuntime/Lambda+Codable.swift
@@ -144,4 +144,37 @@ extension JSONDecoder: LambdaCodableDecoder {}
 extension JSONEncoder: LambdaCodableEncoder {}
 
 extension JSONDecoder: AWSLambdaRuntimeCore.LambdaEventDecoder {}
-extension JSONEncoder: AWSLambdaRuntimeCore.LambdaOutputEncoder {}
+
+@usableFromInline
+package struct LambdaJSONOutputEncoder<Output: Encodable>: LambdaOutputEncoder {
+    @usableFromInline let jsonEncoder: JSONEncoder
+
+    @inlinable
+    package init(_ jsonEncoder: JSONEncoder) {
+        self.jsonEncoder = jsonEncoder
+    }
+
+    @inlinable
+    package func encode(_ value: Output, into buffer: inout ByteBuffer) throws {
+        try self.jsonEncoder.encode(value, into: &buffer)
+    }
+}
+
+extension LambdaCodableAdapter {
+    /// Initializes an instance given an encoder, decoder, and a handler with a non-`Void` output.
+    ///   - Parameters:
+    ///   - encoder: The encoder object that will be used to encode the generic ``Output`` obtained from the `handler`'s `outputWriter` into a ``ByteBuffer``.
+    ///   - decoder: The decoder object that will be used to decode the received ``ByteBuffer`` event into the generic ``Event`` type served to the `handler`.
+    ///   - handler: The handler object.
+    package init(
+        encoder: JSONEncoder,
+        decoder: JSONDecoder,
+        handler: Handler
+    ) where Output: Encodable, Output == Handler.Output, Encoder == LambdaJSONOutputEncoder<Output>, Decoder == JSONDecoder {
+        self.init(
+            encoder: LambdaJSONOutputEncoder(encoder),
+            decoder: decoder,
+            handler: handler
+        )
+    }
+}
