83446a8a60767a61287d1c4d746caac001ede271
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-10-08 21:41:42 +0200

    remove unnecessary license header file

--- Examples/AWSSDK/Package.swift
+++ Examples/AWSSDK/Package.swift
@@ -1,20 +1,4 @@
-// swift-tools-version: 6.0.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the AWS Lambda Swift
-// VSCode extension open source project.
-//
-// Copyright (c) 2024, the VSCode AWS Lambda Swift extension project authors.
-// Licensed under Apache License v2.0.
-//
-// See LICENSE.txt for license information
-// See CONTRIBUTORS.txt for the list of VSCode AWS Lambda Swift project authors
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-//===----------------------------------------------------------------------===//
+// swift-tools-version: 6.0
 
 import PackageDescription
 
