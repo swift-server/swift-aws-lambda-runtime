db17a62e389907f5a0f4ff46c2aa7ff9b69887fb
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-10-08 05:07:47 -0400

    Restructure Examples folder (#387)
    * Restructure Examples folder
    
    * add README
    
    * remove v1 examples
    
    * swift format

---
+++ Examples/HelloWorld/Sources/main.swift
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2024 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import AWSLambdaRuntime
+
+// in this example we are receiving and responding with strings
+
+let runtime = LambdaRuntime {
+    (event: String, context: LambdaContext) in
+    "Hello \(event)"
+}
+
+try await runtime.run()
