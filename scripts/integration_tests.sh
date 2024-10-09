ba369073796c98a48f1d85ffd64f2ee1121ab04f
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-10-09 14:58:45 +0200

    first test to get a matrix setup

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -13,12 +13,38 @@
 ##
 ##===----------------------------------------------------------------------===##
 
-set +x -e
+# set +x -e
 
-for EXAMPLE in $(find Examples -type d -d 1);
-do
-	echo "Building $EXAMPLE"
-	pushd $EXAMPLE
-	LAMBDA_USE_LOCAL_DEPS=../.. swift build
-	popd
-done 
+# for EXAMPLE in $(find Examples -type d -d 1);
+# do
+# 	echo "Building $EXAMPLE"
+# 	pushd $EXAMPLE
+# 	LAMBDA_USE_LOCAL_DEPS=../.. swift build
+# 	popd
+# done 
+
+set -euo pipefail
+
+log() { printf -- "** %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
+fatal() { error "$@"; exit 1; }
+
+test -n "${SWIFT_VERSION:-}" || fatal "SWIFT_VERSION unset"
+test -n "${COMMAND:-}" || fatal "COMMAND unset"
+test -n "${EXAMPLE:-}" || fatal "EXAMPLE unset"
+swift_version="$SWIFT_VERSION"
+command="$COMMAND"
+command_nightly_6_0="$COMMAND_OVERRIDE_NIGHTLY_6_0"
+command_nightly_main="$COMMAND_OVERRIDE_NIGHTLY_MAIN"
+example="$EXAMPLE"
+
+if [[ "$swift_version" == "nightly-6.0" ]] && [[ -n "$command_nightly_6_0" ]]; then
+  log "Running nightly 6.0 command override"
+  eval "$command_nightly_6_0"
+elif [[ "$swift_version" == "nightly-main" ]] && [[ -n "$command_nightly_main" ]]; then
+  log "Running nightly main command override"
+  eval "$command_nightly_main"
+else
+  log "Running default command"
+  eval "$command"
+fi
