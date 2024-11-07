bde3d0d2393d2d343ef0f4055fc626320132fa47
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-11-07 17:06:39 +0100

    ad hellojson sample

---
+++ Examples/HelloJSON/Sources/main.swift
@@ -0,0 +1,43 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2024 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import AWSLambdaRuntime
+import Foundation
+
+// in this example we are receiving and responding with a JSON structure
+
+// the data structure to represent the input parameter
+struct HelloRequest: Decodable {
+    let name: String
+    let age: Int
+}
+
+// the data structure to represent the output response
+struct HelloResponse: Encodable {
+    let greetings: String
+}
+
+// the JSON encoder. It is created outside of the handler.
+// this instance is reused at each invocation.
+let encoder = JSONEncoder()
+
+// the Lambda runtime
+let runtime = LambdaRuntime {
+    (event: HelloRequest, context: LambdaContext) in
+    
+    return HelloResponse(greetings: "Hello \(event.name). You look \(event.age > 30 ? "younger" : "older") than your age.")
+}
+
+// start the loop
+try await runtime.run()
