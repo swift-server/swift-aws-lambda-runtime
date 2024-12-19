4a8f8dc77fdd27e2ad005458db3349b9bd906904
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2024-12-19 13:53:17 +0100

    add license header

--- Examples/CDK/infra/bin/deploy.ts
+++ Examples/CDK/infra/bin/deploy.ts
@@ -1,4 +1,17 @@
-#!/opt/homebrew/opt/node/bin/node
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2024 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import * as cdk from 'aws-cdk-lib';
 import { LambdaApiStack } from '../lib/lambda-api-project-stack';
 
--- Examples/CDK/infra/lib/lambda-api-project-stack.ts
+++ Examples/CDK/infra/lib/lambda-api-project-stack.ts
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2024 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+ 
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigatewayv2';
