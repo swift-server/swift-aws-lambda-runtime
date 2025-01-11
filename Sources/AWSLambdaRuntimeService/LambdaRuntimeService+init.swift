24dc42659fc9841432e04a5adf45c6c29adeb653
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2025-01-11 17:06:35 +0100

    fix logger label + add doc header

--- Sources/AWSLambdaRuntimeService/LambdaRuntimeService+init.swift
+++ Sources/AWSLambdaRuntimeService/LambdaRuntimeService+init.swift
@@ -27,11 +27,12 @@ extension LambdaRuntimeService {
 
     /// Initialize an instance with a `LambdaHandler` defined in the form of a closure **with a non-`Void` return type**.
     /// - Parameters:
+    ///   - logger: The logger object that will be used to log messages. `Logger(label: "LambdaRuntimeService")` used as default.
     ///   - decoder: The decoder object that will be used to decode the incoming `ByteBuffer` event into the generic `Event` type. `JSONDecoder()` used as default.
     ///   - encoder: The encoder object that will be used to encode the generic `Output` into a `ByteBuffer`. `JSONEncoder()` used as default.
     ///   - body: The handler in the form of a closure.
     public convenience init<Event: Decodable, Output>(
-        logger: Logger = Logger(label: "SLambdaRuntimeService"),
+        logger: Logger = Logger(label: "LambdaRuntimeService"),
         decoder: JSONDecoder = JSONDecoder(),
         encoder: JSONEncoder = JSONEncoder(),
         body: sending @escaping (Event, LambdaContext) async throws -> Output
@@ -55,10 +56,12 @@ extension LambdaRuntimeService {
     }
 
     /// Initialize an instance with a `LambdaHandler` defined in the form of a closure **with a `Void` return type**.
-    /// - Parameter body: The handler in the form of a closure.
-    /// - Parameter decoder: The decoder object that will be used to decode the incoming `ByteBuffer` event into the generic `Event` type. `JSONDecoder()` used as default.
+    /// - Parameters:
+    ///   - logger: The logger object that will be used to log messages. `Logger(label: "LambdaRuntimeService")` used as default.
+    ///   - decoder: The decoder object that will be used to decode the incoming `ByteBuffer` event into the generic `Event` type. `JSONDecoder()` used as default.
+    ///   - body: The handler in the form of a closure.
     public convenience init<Event: Decodable>(
-        logger: Logger = Logger(label: "SLambdaRuntimeService"),
+        logger: Logger = Logger(label: "LambdaRuntimeService"),
         decoder: JSONDecoder = JSONDecoder(),
         body: sending @escaping (Event, LambdaContext) async throws -> Void
     )
