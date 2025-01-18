799af6a42dcbff0970cbc27602be1c6bb5d7e54d
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2025-01-18 23:13:06 +0100

    fix ci

--- Examples/ResourcesPackaging/Package.swift
+++ Examples/ResourcesPackaging/Package.swift
@@ -3,6 +3,9 @@
 
 import PackageDescription
 
+// needed for CI to test the local version of the library
+import struct Foundation.URL
+
 let package = Package(
     name: "ResourcesPackaging",
     platforms: [.macOS(.v15)],
@@ -25,3 +28,30 @@ let package = Package(
         )
     ]
 )
+
+if let localDepsPath = Context.environment["LAMBDA_USE_LOCAL_DEPS"],
+    localDepsPath != "",
+    let v = try? URL(fileURLWithPath: localDepsPath).resourceValues(forKeys: [.isDirectoryKey]),
+    v.isDirectory == true
+{
+    // when we use the local runtime as deps, let's remove the dependency added above
+    let indexToRemove = package.dependencies.firstIndex { dependency in
+        if case .sourceControl(
+            name: _,
+            location: "https://github.com/swift-server/swift-aws-lambda-runtime.git",
+            requirement: _
+        ) = dependency.kind {
+            return true
+        }
+        return false
+    }
+    if let indexToRemove {
+        package.dependencies.remove(at: indexToRemove)
+    }
+
+    // then we add the dependency on LAMBDA_USE_LOCAL_DEPS' path (typically ../..)
+    print("[INFO] Compiling against swift-aws-lambda-runtime located at \(localDepsPath)")
+    package.dependencies += [
+        .package(name: "swift-aws-lambda-runtime", path: localDepsPath)
+    ]
+}
