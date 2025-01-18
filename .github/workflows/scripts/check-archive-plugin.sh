86149fddc18db95ad349cf89d50a93665be5720e
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2025-01-18 22:33:31 +0100

    add integration test to check the resources inside the package

--- .github/workflows/scripts/check-archive-plugin.sh
+++ .github/workflows/scripts/check-archive-plugin.sh
@@ -13,25 +13,40 @@
 ##
 ##===----------------------------------------------------------------------===##
 
-EXAMPLE=HelloWorld
-OUTPUT_DIR=.build/plugins/AWSLambdaPackager/outputs/AWSLambdaPackager
-OUTPUT_FILE=${OUTPUT_DIR}/MyLambda/bootstrap
-ZIP_FILE=${OUTPUT_DIR}/MyLambda/MyLambda.zip
+check_archive_plugin() {
+    local EXAMPLE=$1
+    OUTPUT_DIR=.build/plugins/AWSLambdaPackager/outputs/AWSLambdaPackager
+    OUTPUT_FILE=${OUTPUT_DIR}/MyLambda/bootstrap
+    ZIP_FILE=${OUTPUT_DIR}/MyLambda/MyLambda.zip
 
-pushd Examples/${EXAMPLE} || exit 1
+    pushd Examples/${EXAMPLE} || exit 1
 
-# package the example (docker and swift toolchain are installed on the GH runner)
-LAMBDA_USE_LOCAL_DEPS=../.. swift package archive --allow-network-connections docker || exit 1
+    # package the example (docker and swift toolchain are installed on the GH runner)
+    LAMBDA_USE_LOCAL_DEPS=../.. swift package archive --allow-network-connections docker || exit 1
 
-# did the plugin generated a Linux binary?
-[ -f "${OUTPUT_FILE}" ]
-file "${OUTPUT_FILE}" | grep --silent ELF
+    # did the plugin generated a Linux binary?
+    [ -f "${OUTPUT_FILE}" ]
+    file "${OUTPUT_FILE}" | grep --silent ELF
 
-# did the plugin created a ZIP file?
-[ -f "${ZIP_FILE}" ]
+    # did the plugin created a ZIP file?
+    [ -f "${ZIP_FILE}" ]
 
-# does the ZIP file contain the bootstrap?
-unzip -l "${ZIP_FILE}" | grep --silent bootstrap
+    # does the ZIP file contain the bootstrap?
+    unzip -l "${ZIP_FILE}" | grep --silent bootstrap
 
-echo "✅ The archive plugin is OK"
-popd || exit 1
+    # if EXAMPLE is ResourcesPackaging, check if the ZIP file contains hello.txt
+    if [ "$EXAMPLE" == "ResourcesPackaging" ]; then
+        unzip -l "${ZIP_FILE}" | grep --silent hello.txt
+    fi    
+
+    echo "✅ The archive plugin is OK with example ${EXAMPLE}"
+    popd || exit 1
+}
+
+# List of examples
+EXAMPLES=("HelloWorld" "ResourcesPackaging")
+
+# Iterate over each example and call check_archive_plugin
+for EXAMPLE in "${EXAMPLES[@]}"; do
+  check_archive_plugin "$EXAMPLE"
+done
