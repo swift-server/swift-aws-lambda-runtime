438f4cb2f877ea88f75e7cc2801d7c3b937c87cf
Fabian Fett <fabianfett@apple.com>
2025-03-05 15:57:07 +0100

    Fix header

--- Sources/AWSLambdaRuntime/LambdaRuntime+ServiceLifecycle.swift
+++ Sources/AWSLambdaRuntime/LambdaRuntime+ServiceLifecycle.swift
@@ -1,9 +1,16 @@
+//===----------------------------------------------------------------------===//
 //
-//  LambdaRuntime+ServiceLifecycle.swift
-//  swift-aws-lambda-runtime
+// This source file is part of the SwiftAWSLambdaRuntime open source project
 //
-//  Created by Fabian Fett on 01.03.25.
+// Copyright (c) 2025 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
 //
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 #if ServiceLifecycleSupport
 import ServiceLifecycle
