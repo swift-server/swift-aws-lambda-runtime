4d3479a9f61fb13d500d7744f1c17a1d3a5a2ca6
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2025-07-07 12:13:12 +0200

    license header

--- Tests/AWSLambdaRuntimeTests/LambdaRuntime+ServiceLifeCycle.swift
+++ Tests/AWSLambdaRuntimeTests/LambdaRuntime+ServiceLifeCycle.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2024 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 #if ServiceLifecycleSupport
 @testable import AWSLambdaRuntime
 import ServiceLifecycle
