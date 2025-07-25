748c1fb5401ee4cf6bb23f7d51fdb0039a8d6743
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-07-25 17:57:19 +0400

    fix license header

--- scripts/check-doc.sh
+++ scripts/check-doc.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 ##===----------------------------------------------------------------------===##
 ##
+## This source file is part of the SwiftAWSLambdaRuntime open source project
+##
+## Copyright (c) 2025 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+##===----------------------------------------------------------------------===##
+##
 ## This source file is part of the Swift.org open source project
 ##
 ## Copyright (c) 2024 Apple Inc. and the Swift project authors
