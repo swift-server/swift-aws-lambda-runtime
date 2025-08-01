70c0af163c04d52a636a512aa85880118570937f
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-08-01 07:31:07 +0400

    Simplify deadlineDate

--- Sources/AWSLambdaRuntime/FoundationSupport/Context+Foundation.swift
+++ Sources/AWSLambdaRuntime/FoundationSupport/Context+Foundation.swift
@@ -20,9 +20,11 @@ import struct Foundation.Date
 #endif
 
 extension LambdaContext {
+    /// Returns the deadline as a Date for the Lambda function execution.
+    /// I'm not sure how usefull it is to have this as a Date, with only seconds precision,
+    /// but I leave it here for compatibility with the FoundationJSONSupport trait.
     var deadlineDate: Date {
-        let secondsSinceEpoch = Double(self.deadline.milliseconds()) / -1_000_000_000
-        return Date(timeIntervalSince1970: secondsSinceEpoch)
+        Date(timeIntervalSince1970: Double(self.deadline.milliseconds()) / 1000)
     }
 }
 #endif  // trait: FoundationJSONSupport
