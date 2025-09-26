d8aa38be785db3289ec01b4048564369e2d4a6c2
Erin Sparling <erin@everyplace.net>
2025-09-26 08:13:48 -0400

    [Example] Add APIGatewayV1 example (#569)
    This PR adds an APIGateway V1 example, which differs slightly from the
    existing [V2
    example](https://github.com/swift-server/swift-aws-lambda-runtime/tree/main/Examples/APIGateway).
    
    ### Motivation:
    
    While APIGatewayV2 has existed for a while, there are still some
    scenarios where V1 is preferable (see [Choose between REST APIs and HTTP
    APIs](https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-vs-rest.html)
    for more details). This PR adds a working example for the `Api` endpoint
    type.
    
    ### Modifications:
    
    Sources/main.swift
    - Swapped APIGatewayRequestV2 and APIGatewayResponseV2 for their
    unversioned counterparts (aka V1).
    
    template.yaml
    - Changed the Events name and corresponding `Type: Api` values.
    - Added necessary additional properties to define a `GET` endpoint for
    an `Api` endpoint.
    
    Readme.md
    - Updated the request and response example outputs.
    
    ### Result:
    
    Now, an APIGateway and APIGatewayV1 example exist. 
    
    > [!NOTE]
    > It is the author's opinion that this new example should technically be
    called `APIGateway` and the existing one should be renamed to
    `APIGatewayV2` to keep consistent with the request and response type
    naming conventions, but this is omitted from this request to be less
    disruptive.

---
+++ Examples/APIGatewayV1/Package.swift
@@ -0,0 +1,56 @@
+// swift-tools-version:6.2
+
+import PackageDescription
+
+// needed for CI to test the local version of the library
+import struct Foundation.URL
+
+let package = Package(
+    name: "swift-aws-lambda-runtime-example",
+    platforms: [.macOS(.v15)],
+    products: [
+        .executable(name: "APIGatewayLambda", targets: ["APIGatewayLambda"])
+    ],
+    dependencies: [
+        // during CI, the dependency on local version of swift-aws-lambda-runtime is added dynamically below
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "2.0.0"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", from: "1.0.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "APIGatewayLambda",
+            dependencies: [
+                .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+                .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-events"),
+            ],
+            path: "Sources"
+        )
+    ]
+)
+
+if let localDepsPath = Context.environment["LAMBDA_USE_LOCAL_DEPS"],
+    localDepsPath != "",
+    let v = try? URL(fileURLWithPath: localDepsPath).resourceValues(forKeys: [.isDirectoryKey]),
+    v.isDirectory == true
+{
+    // when we use the local runtime as deps, let's remove the dependency added above
+    let indexToRemove = package.dependencies.firstIndex { dependency in
+        if case .sourceControl(
+            name: _,
+            location: "https://github.com/swift-server/swift-aws-lambda-runtime.git",
+            requirement: _
+        ) = dependency.kind {
+            return true
+        }
+        return false
+    }
+    if let indexToRemove {
+        package.dependencies.remove(at: indexToRemove)
+    }
+
+    // then we add the dependency on LAMBDA_USE_LOCAL_DEPS' path (typically ../..)
+    print("[INFO] Compiling against swift-aws-lambda-runtime located at \(localDepsPath)")
+    package.dependencies += [
+        .package(name: "swift-aws-lambda-runtime", path: localDepsPath)
+    ]
+}
