8168a5c22ed568595d6c9f7bd8a51de69b2b89ab
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-10-20 20:01:46 +0200

    fix ci : check libFoundation on HelloWorld example (#593)
    Fix errors in the CI
    The script that checks the presence of `libFoundation` in the binary
    started to fail.
    I can't think about a recent change that would cause this.
    
    This PR change the test script to use the `HelloWorld` example instead
    of `APIGAtewayV2`
    
    Co-authored-by: Sebastien Stormacq <stormacq@amazon.lu>

--- .github/workflows/scripts/check-link-foundation.sh
+++ .github/workflows/scripts/check-link-foundation.sh
@@ -17,23 +17,23 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
-EXAMPLE=APIGatewayV2
+EXAMPLE=HelloWorld
 OUTPUT_DIR=.build/release
-OUTPUT_FILE=${OUTPUT_DIR}/APIGatewayLambda
+OUTPUT_FILE=${OUTPUT_DIR}/MyLambda
 LIBS_TO_CHECK="libFoundation.so libFoundationInternationalization.so lib_FoundationICU.so"
 
 pushd Examples/${EXAMPLE} || fatal "Failed to change directory to Examples/${EXAMPLE}."
 
 # recompile the example without the --static-swift-stdlib flag
-LAMBDA_USE_LOCAL_DEPS=../.. swift build -c release -Xlinker -s || fatal "Failed to build the example."
+LAMBDA_USE_LOCAL_DEPS=../.. swift build -c release || fatal "Failed to build the example."
 
 # check if the binary exists
 if [ ! -f "${OUTPUT_FILE}" ]; then
-  error "❌ ${OUTPUT_FILE} does not exist."
+  fatal "❌ ${OUTPUT_FILE} does not exist."
 fi
 
 # Checking for Foundation or ICU dependencies
-echo "Checking for Foundation or ICU dependencies in ${OUTPUT_DIR}/${OUTPUT_FILE}."
+echo "Checking for Foundation or ICU dependencies in ${OUTPUT_FILE}."
 LIBRARIES=$(ldd ${OUTPUT_FILE} | awk '{print $1}')
 for LIB in ${LIBS_TO_CHECK}; do
   echo -n "Checking for ${LIB}... "
